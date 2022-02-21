3a42a0591fa3f128e02f8dc56f2ac13a7c3b2737
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-21 10:30:03 -0500

    note creation functionality

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 //import variables
 const express = require('express');
 const { notes }= require('./db/db.json');
+const fs = require('fs')
+const path = require('path');
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 // function that filters through an array by the note's id
 function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
+};
+
+// function that allows new note creation from client
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray}, null, 4)
+  );
+  
+  return note;
+}
+
+// function that validates new entries into the notes array
+function validateNote(note) {
+  if(!note.title) {
+    return false;
+  }
+  if(!note.text) {
+    return false;
+  }
+  return true;
 }
 
 // get all notes 
@@ -24,8 +52,8 @@ app.get('/api/notes/:id', (req, res) => {
   console.log(result)
 })
 
-// if no entry exists for that record, return the error
-app.get('/api/animals/:id', (req, res) => {
+// if no entry exists for that record, return an error
+app.get('/api/notes/:id', (req, res) => {
   const result = findById(req.params.id, notes);
   if (result) {
     res.json(result);
@@ -33,6 +61,20 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
   }
 })
+
+// accept data to be used or stored server-side
+app.post('/api/notes', (req, res) => {
+  // set id based on the next index
+  req.body.id = notes.length.toString();
+  // if note is missing inforrmation, send back 400 errorr
+  if(!validateNote(req.body)) {
+    res.status(400).send('The title or text is missing from this note.');
+  } else {
+    // add note to json file through the notes array
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 // listener
 app.listen(3002, () => {
