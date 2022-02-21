a361841275b83d1e745003e6b4665e7a6ea365a6
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-21 15:01:56 -0500

    fix routes

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 //middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
 
 // listener
 app.listen(PORT, () => {
